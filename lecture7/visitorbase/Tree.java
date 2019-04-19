db3d5381effb3dc66547134604d8ad3d590e0957
Alexander <alexander_agam@hotmail.com>
2019-04-19 22:51:57 +0200

    funkar åtminstånde

--- lecture7/visitorbase/Tree.java
+++
@@ -1,5 +0,0 @@
-package Oop_a2.gitK.ass2.lecture7.visitorbase;
-
-public interface Tree<T> {
-	<R> R accept(TreeVisitor<T, R> v);
-}
--- lecture7/visitorbase/TreeVisitor.java
+++
@@ -1,9 +0,0 @@
-package Oop_a2.gitK.ass2.lecture7.visitorbase;
-
-public interface TreeVisitor<T, R> {
-	R visit(Tree<T> t);
-
-	R visit(Leaf<T> l);
-
-	R visit(Node<T> n);
-}
